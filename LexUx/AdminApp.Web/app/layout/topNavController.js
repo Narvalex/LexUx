c52823a9dfda15d80a31b5386ba9e5d4e9c15be5
Alexis Narváez <narvalex@hotmail.com>
2015-11-02 16:28:22 -0300

    Now the animations are responsive too!

--- LexUx/AdminApp.Web/app/layout/topNavController.js
+++ LexUx/AdminApp.Web/app/layout/topNavController.js
@@ -1,14 +1,37 @@
 ﻿(function () {
     'use strict';
-    angular.module('app').controller('topNavController', ['$rootScope', topNavController]);
 
-    function topNavController($rootScope) {
+    angular.module('app').controller('topNavController',
+        ['$rootScope', '$scope', '$timeout', topNavController]);
+
+    function topNavController($rootScope, $scope, $timeout) {
         var root = $rootScope;
+        var vm = $scope;
+
+        vm.collapsed = true;
+
+        vm.onCollapseButtonClicked = onCollapseButtonClicked;
+        vm.collapseIfApplicable = collapseIfApplicable;
 
         activate();
 
         function activate() {
             root.showSplashPage = false;
+        }
+
+        function onCollapseButtonClicked() {
+            vm.collapsed = !vm.collapsed;
+        }
+
+        function collapseIfApplicable() {
+            if (vm.collapsed) {
+                return;
+            }
+
+            // collapse topnav click
+            $timeout(function () {
+                document.getElementById('collapseTopnavButton').click();
+            }); 
         }
     }
 })();
